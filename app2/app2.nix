0067630ddd19443cd737f73295c8272532d37927
Kaan Caglan <caglankaan@gmail.com>
2025-03-18 16:14:07 +0300

    chore(appv2): fixed app2.nix
    Signed-off-by: Kaan Caglan <caglankaan@gmail.com>

--- app2/app2.nix
+++ app2/app2.nix
@@ -20,7 +20,7 @@ _: {
     {
       packages = {
         app2 = jsPkgs.buildNpmPackage {
-          npmDepsHash = "sha256-Jc9GX9R+5n5Ce+xPPUH1MfY7/3o2hwn/aP3V6yi9acY=";
+          npmDepsHash = "sha256-wazkS8shw0R2/eSj/2JfwaLdO9pHDfNMd1T4Pc1gphQ=";
           src = ./.;
           sourceRoot = "app2";
           npmFlags = [ "--legacy-peer-deps" ];
